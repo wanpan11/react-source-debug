83358a5bba5be04c5dbf6f03ff8a3ee8c01a55e7
Wanpan <wanpan96@163.com>
2023-09-12 18:42:35 +0800

    update

--- demo/src/App_context.jsx
+++ demo/src/App_context.jsx
@@ -1,33 +1,25 @@
 import React, { createContext, useContext } from "react";
 
-const TestContext = createContext({ value: 0 });
+const TestContext = createContext({ value: "@@@" });
 
 const Detail_1 = () => {
   const { value } = useContext(TestContext);
 
-  return (
-    <span style={{ marginBottom: "12px" }}>
-      <button>Provider：{value}</button>
-    </span>
-  );
+  return <button>Detail_1：{value}</button>;
 };
 
 const Detail_2 = () => {
   const { value } = useContext(TestContext);
 
-  return (
-    <span style={{ marginBottom: "12px" }}>
-      <button>Provider：{value}</button>
-    </span>
-  );
+  return <button>Detail_2：{value}</button>;
 };
 
 const App = () => {
   return (
     <ul>
-      <h1>Provider</h1>
+      <h1>我是标题呀</h1>
 
-      <TestContext.Provider value={{ value: 1 }}>
+      <TestContext.Provider value={{ value: "init" }}>
         <Detail_1 />
       </TestContext.Provider>
 
