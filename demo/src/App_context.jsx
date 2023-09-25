24add354aa4cf4e3bb1e053e7d3ca652e03f9ec2
Wanpan <wanpan96@163.com>
2023-09-25 16:34:45 +0800

    update

--- demo/src/App_context.jsx
+++ demo/src/App_context.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const TestContext = createContext({ value: "@@@" });
+const TestContext = createContext({ value: "__init__" });
 
 const Detail_1 = () => {
   const { value } = useContext(TestContext);
@@ -15,15 +15,27 @@ const Detail_2 = () => {
 };
 
 const App = () => {
+  const [state, setState] = useState(0);
+
   return (
     <ul>
       <h1>我是标题呀</h1>
 
-      <TestContext.Provider value={{ value: "init" }}>
+      <TestContext.Provider value={{ value: "update" }}>
         <Detail_1 />
       </TestContext.Provider>
 
       <Detail_2 />
+
+      <h1 />
+
+      <button
+        onClick={() => {
+          setState(state + 1);
+        }}
+      >
+        setState + 1 = {state}
+      </button>
     </ul>
   );
 };
